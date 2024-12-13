b78df4d4ecc2561f2d820578c720a6164f538951
xiewen <1123654054@qq.com>
2024-12-13 13:38:03 +0800

    用户登录页面布局完成

--- ivue-admin/src/router/index.ts
+++ ivue-admin/src/router/index.ts
@@ -1,6 +1,11 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 
-const routes: Array<RouteRecordRaw> = [];
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/login',
+    component: () => import('@/views/login/index.vue')
+  }
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
