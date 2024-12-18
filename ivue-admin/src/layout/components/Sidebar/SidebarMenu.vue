9cb1804465d53f708ccf011acc02887756d14084
xiewen <1123654054@qq.com>
2024-12-18 20:12:53 +0800

    新增页面组件

---
+++ ivue-admin/src/layout/components/Sidebar/SidebarMenu.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-menu unique-opened background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+    <el-sub-menu index="1">
+      <template #title>
+        <el-icon>
+          <InfoFilled />
+        </el-icon>
+        <span>导航一</span>
+      </template>
+      <el-menu-item index="1-1">选项1</el-menu-item>
+      <el-menu-item index="1-2">选项2</el-menu-item>
+    </el-sub-menu>
+    <el-sub-menu index="2">
+      <template #title>
+        <el-icon>
+          <InfoFilled />
+        </el-icon>
+        <span>导航四</span>
+      </template>
+    </el-sub-menu>
+  </el-menu>
+</template>
+
+<script setup lang="ts">
+import { InfoFilled } from '@element-plus/icons-vue';
+</script>
+
+<style scoped></style>
--- ivue-admin/src/layout/components/Sidebar/index.vue
+++ ivue-admin/src/layout/components/Sidebar/index.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
-    SideBar
+    <h1>占位</h1>
+    <el-scrollbar>
+      <SidebarMenu />
+    </el-scrollbar>
   </div>
 </template>
 
 <script setup lang="ts">
-
+import SidebarMenu from './SidebarMenu.vue';
 </script>
 
 <style scoped></style>
